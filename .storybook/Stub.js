e1688afc4f71ba7ac706cba15bb4fc94118d01e3
keik <keik@c-fo.com>
2020-04-30 07:02:40 +0900

    :+1: Stub container component in storybook but not storyshots.

---
+++ .storybook/Stub.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const Stub = () => (
+  <div
+    style={{
+      border: "2px solid red",
+      padding: "1rem",
+    }}
+  >
+    Stubbed container component
+  </div>
+);
+
+export default Stub;
